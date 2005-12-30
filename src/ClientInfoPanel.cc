703423a7bad13f389314e027831044e8ccbe3b5c
Chris Wilson <chris+github@qwirx.com>
2005-12-30 00:30:54 +0000

    * src/ClientInfoPanel.cc - Tried to make configuration status label auto-wrap (not working yet) - Removed unused IDs for bound controls
    git-svn-id: https://svn.code.sf.net/p/boxi/code/trunk/boxi@179 bde4d9af-c013-0410-a270-fa18cafdf79f

--- src/ClientInfoPanel.cc
+++ src/ClientInfoPanel.cc
@@ -44,6 +44,23 @@ void LoadBitmap(const struct StaticImage& rImageData, wxBitmap& rDest)
 	rDest = wxBitmap(img);
 }
 
+class WrappingLabel : public wxStaticText
+{
+	public:
+	WrappingLabel(wxWindow* pParent, wxWindowID id, const wxString& rLabel)
+	: wxStaticText(pParent, id, rLabel) { }
+	virtual void SetLabel(const wxString& rLabel)
+	{
+		this->wxStaticText::SetLabel(rLabel);
+		wxWindow* pTarget = GetParent();
+		while (!pTarget->IsTopLevel())
+		{
+			pTarget = pTarget->GetParent();
+		}
+		pTarget->Layout();
+	}
+};
+
 ClientInfoPanel::ClientInfoPanel(ClientConfig *pConfig,
 	wxWindow* parent, wxWindowID id,
 	const wxPoint& pos, const wxSize& size,
@@ -56,6 +73,15 @@ ClientInfoPanel::ClientInfoPanel(ClientConfig *pConfig,
 	wxBoxSizer *pTabSizer = new wxBoxSizer( wxVERTICAL );
 	SetSizer(pTabSizer);
 	
+	wxBoxSizer* pStateSizer = new wxBoxSizer( wxHORIZONTAL );
+	pTabSizer->Add(pStateSizer, 0, wxGROW | wxALL, 4);
+
+	mpConfigStateBitmap = new TickCrossIcon(this);
+	pStateSizer->Add(mpConfigStateBitmap, 0, wxALIGN_CENTER_VERTICAL | wxALL, 4);
+
+	mpConfigStateLabel = new WrappingLabel(this, -1, wxT("Unknown state"));
+	pStateSizer->Add(mpConfigStateLabel, 1, wxALIGN_CENTER_VERTICAL | wxALL, 4);
+	
 	wxScrolledWindow *pVisibleArea = new wxScrolledWindow(this, wxID_ANY,
 		wxDefaultPosition, wxDefaultSize, wxVSCROLL);
 	pTabSizer->Add(pVisibleArea, 1, wxGROW | wxALL, 4);
@@ -63,15 +89,6 @@ ClientInfoPanel::ClientInfoPanel(ClientConfig *pConfig,
 	wxBoxSizer *pScrollablePanelSizer = new wxBoxSizer( wxVERTICAL );
 	pVisibleArea->SetSizer(pScrollablePanelSizer);
 	pVisibleArea->SetScrollRate(0, 1);
-	
-	wxBoxSizer* pStateSizer = new wxBoxSizer( wxHORIZONTAL );
-	pScrollablePanelSizer->Add(pStateSizer, 0, wxGROW | wxALL, 4);
-
-	mpConfigStateBitmap = new TickCrossIcon(pVisibleArea);
-	pStateSizer->Add(mpConfigStateBitmap, 0, wxGROW | wxALL, 4);
-
-	mpConfigStateLabel = new wxStaticText(pVisibleArea, -1, wxT("Unknown state"));
-	pStateSizer->Add(mpConfigStateLabel, 1, wxGROW | wxALL, 4);
 	
 	wxNotebook *pClientPropsNotebook = new wxNotebook(pVisibleArea, -1);
 	pScrollablePanelSizer->Add(pClientPropsNotebook, 1, 
@@ -85,86 +102,85 @@ ClientInfoPanel::ClientInfoPanel(ClientConfig *pConfig,
 
 	mpStoreHostnameCtrl = pBasicPanel->AddParam(
 		wxT("Store Host:"), pConfig->StoreHostname, 
-		ID_ClientProp_StoreHostname, 
-		FALSE, FALSE, NULL);
+		wxID_ANY, FALSE, FALSE, NULL);
 
 	mpAccountNumberCtrl = pBasicPanel->AddParam(
 		wxT("Account Number:"), pConfig->AccountNumber, 
-		"0x%x", ID_ClientProp_AccountNumber);
+		"0x%x", wxID_ANY);
 		
 	mpKeysFileCtrl = pBasicPanel->AddParam(
 		wxT("Keys File:"), pConfig->KeysFile, 
-		ID_ClientProp_KeysFile, TRUE, FALSE, 
+		wxID_ANY, TRUE, FALSE, 
 		wxT("Encryption key files (*-FileEncKeys.raw)|"
 			"*-FileEncKeys.raw"));
 		
 	mpCertificateFileCtrl = pBasicPanel->AddParam(
 		wxT("Certificate File:"), pConfig->CertificateFile, 
-		ID_ClientProp_CertificateFile, TRUE, FALSE,
+		wxID_ANY, TRUE, FALSE,
 		wxT("Certificate files (*-cert.pem)|*-cert.pem"));
 
 	mpPrivateKeyFileCtrl = pBasicPanel->AddParam(
 		wxT("Private Key File:"), pConfig->PrivateKeyFile, 
-		ID_ClientProp_PrivateKeyFile, TRUE, FALSE,
+		wxID_ANY, TRUE, FALSE,
 		wxT("Private key files (*-key.pem)|*-key.pem"));
 
 	mpTrustedCAsFileCtrl = pBasicPanel->AddParam(
 		wxT("Trusted CAs File:"), pConfig->TrustedCAsFile, 
-		ID_ClientProp_TrustedCAsFile, TRUE, FALSE,
+		wxID_ANY, TRUE, FALSE,
 		wxT("Server CA certificate (serverCA.pem)|serverCA.pem"));
 
 	mpDataDirectoryCtrl = pBasicPanel->AddParam(
 		wxT("Data Directory:"), pConfig->DataDirectory, 
-		ID_ClientProp_DataDirectory, FALSE, TRUE, NULL);
+		wxID_ANY, FALSE, TRUE, NULL);
 
 	mpCommandSocketCtrl = pBasicPanel->AddParam(
 		wxT("Command Socket:"), pConfig->CommandSocket, 
-		ID_ClientProp_CommandSocket, TRUE, FALSE, 
+		wxID_ANY, TRUE, FALSE, 
 		wxT("Command socket (bbackupd.sock)|bbackupd.sock"));
 
 	mpExtendedLoggingCtrl = pBasicPanel->AddParam(
 		wxT("Extended Logging:"), pConfig->ExtendedLogging, 
-		ID_ClientProp_ExtendedLogging);
+		wxID_ANY);
 
 	mpUpdateStoreIntervalCtrl = pAdvancedPanel->AddParam(
 		wxT("Update Store Interval:"), pConfig->UpdateStoreInterval, 
-		"%d", ID_ClientProp_UpdateStoreInterval);
+		"%d", wxID_ANY);
 		
 	mpMinimumFileAgeCtrl = pAdvancedPanel->AddParam(
 		wxT("Minimum File Age:"), pConfig->MinimumFileAge, 
-		"%d", ID_ClientProp_MinimumFileAge);
+		"%d", wxID_ANY);
 
 	mpMaxUploadWaitCtrl = pAdvancedPanel->AddParam(
 		wxT("Maximum Upload Wait:"), 
-		pConfig->MaxUploadWait, "%d", ID_ClientProp_MaxUploadWait);
+		pConfig->MaxUploadWait, "%d", wxID_ANY);
 
 	mpFileTrackingSizeThresholdCtrl = pAdvancedPanel->AddParam(
 		wxT("File Tracking Size Threshold:"), 
 		pConfig->FileTrackingSizeThreshold, 
-		"%d", ID_ClientProp_FileTrackingSizeThreshold);
+		"%d", wxID_ANY);
 		
 	mpDiffingUploadSizeThresholdCtrl = pAdvancedPanel->AddParam(
 		wxT("Diffing Upload Size Threshold:"), 
 		pConfig->DiffingUploadSizeThreshold, 
-		"%d", ID_ClientProp_DiffingUploadSizeThreshold);
+		"%d", wxID_ANY);
 
 	mpMaximumDiffingTimeCtrl = pAdvancedPanel->AddParam(
 		wxT("Maximum Diffing Time:"), pConfig->MaximumDiffingTime, 
-		"%d", ID_ClientProp_MaximumDiffingTime);
+		"%d", wxID_ANY);
 
 	mpNotifyScriptCtrl = pAdvancedPanel->AddParam(
 		wxT("Notify Script:"), pConfig->NotifyScript, 
-		ID_ClientProp_NotifyScript, TRUE, FALSE,
+		wxID_ANY, TRUE, FALSE,
 		wxT("Default script (NotifySysadmin.sh)|NotifySysadmin.sh|"
 			"All files (*)|*"));
 	
 	mpSyncAllowScriptCtrl = pAdvancedPanel->AddParam(
 		wxT("Sync Allow Script:"), pConfig->SyncAllowScript, 
-		ID_ClientProp_SyncAllowScript, TRUE, FALSE,
+		wxID_ANY, TRUE, FALSE,
 		wxT("All files (*)|*"));
 
 	mpPidFileCtrl = pAdvancedPanel->AddParam(wxT("Process ID File:"), 
-		pConfig->PidFile, ID_ClientProp_PidFile, TRUE, FALSE,
+		pConfig->PidFile, wxID_ANY, TRUE, FALSE,
 		wxString(wxT("Client PID files (bbackupd.pid)|bbackupd.pid")));
 	
 	wxSizer* pActionCtrlSizer = new wxBoxSizer(wxHORIZONTAL);
