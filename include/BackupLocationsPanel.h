2fc9ac429a65f2a0ef96e97552557f0f6d06e3ad
Chris Wilson <chris+github@qwirx.com>
2006-01-28 02:12:10 +0000

    * include/BackupLocationsPanel.h - Declare MainFrame rather than including MainFrame.h
    git-svn-id: https://svn.code.sf.net/p/boxi/code/trunk/boxi@231 bde4d9af-c013-0410-a270-fa18cafdf79f

--- include/BackupLocationsPanel.h
+++ include/BackupLocationsPanel.h
@@ -34,7 +34,9 @@
 
 class BackupTreeNode;
 class BackupTreeCtrl;
+class ClientConfig;
 class LocationsPanel;
+class MainFrame;
 
 class BackupLocationsPanel : public wxPanel, public ConfigChangeListener 
 {
