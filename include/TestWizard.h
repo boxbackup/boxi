646306bb95d4bc7e5980c529a242764634cd9e2a
Chris Wilson <chris+github@qwirx.com>
2006-08-13 23:35:45 +0000

    * include/TestWizard.h - Moved TestWizard class definition to a header file
    git-svn-id: https://svn.code.sf.net/p/boxi/code/trunk/boxi@314 bde4d9af-c013-0410-a270-fa18cafdf79f

---
+++ include/TestWizard.h
@@ -0,0 +1,65 @@
+/***************************************************************************
+ *            TestWizard.h
+ *
+ *  Sat Aug 12 14:34:26 2006
+ *  Copyright  2006  Chris Wilson
+ *  chris-boxisource@qwirx.com
+ ****************************************************************************/
+
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+ 
+#ifndef _TESTWIZARD_H
+#define _TESTWIZARD_H
+
+#include <openssl/x509.h>
+
+#include <wx/filename.h>
+
+#include "TestFrame.h"
+
+class TestWizard : public TestWithConfig
+{
+	public:
+	TestWizard() { }
+	virtual void RunTest();
+	static CppUnit::Test *suite();
+
+	private:
+	wxButton*    mpForwardButton;
+	SetupWizard* mpWizard;
+
+	void CheckForwardErrorImpl(message_t messageId, 
+		const wxString& rMessageName, const CppUnit::SourceLine& rLine);
+	X509* LoadCertificate(const wxFileName& rCertFile);
+	void SignRequestWithKey
+	(
+		const wxFileName& rRequestFile, 
+		const wxFileName& rKeyFile, 
+		const wxFileName& rIssuerCertFile, 
+		const wxFileName& rCertFile
+	);
+	void ClickForward()
+	{
+		ClickButtonWaitEvent(ID_Setup_Wizard_Frame, wxID_FORWARD);
+	}
+	void ClickBackward()
+	{
+		ClickButtonWaitEvent(ID_Setup_Wizard_Frame, wxID_BACKWARD);
+	}
+};
+
+#endif /* _TESTWIZARD_H */
