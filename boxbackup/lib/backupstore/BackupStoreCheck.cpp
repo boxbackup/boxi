96a03e5a58f6b49c6d9b6a4aa5f9c534f26c9d65
Chris Wilson <chris+github@qwirx.com>
2006-08-27 22:17:16 +0000

    * boxbackup/lib/backupstore/BackupStoreCheck.cpp - Reuse existing variable rather than duplicating it
    git-svn-id: https://svn.code.sf.net/p/boxi/code/trunk/boxi@373 bde4d9af-c013-0410-a270-fa18cafdf79f

--- boxbackup/lib/backupstore/BackupStoreCheck.cpp
+++ boxbackup/lib/backupstore/BackupStoreCheck.cpp
@@ -466,11 +466,10 @@ bool BackupStoreCheck::CheckAndAddObject(int64_t ObjectID, const std::string &rF
 		}
 		
 		// Add to usage counts
-		int64_t s = file->GetDiscUsageInBlocks();
-		mBlocksUsed += s;
+		mBlocksUsed += size;
 		if(!isFile)
 		{
-			mBlocksInDirectories += s;
+			mBlocksInDirectories += size;
 		}
 	}
 	catch(...)
@@ -741,5 +740,3 @@ void BackupStoreCheck::CheckDirectories()
 	}
 
 }
-
-
