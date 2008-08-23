ce6f8c831b2ee2f11c5795872127e20fb3806236
Chris Wilson <chris+github@qwirx.com>
2008-08-23 22:41:35 +0000

    Add a class for wxFileDialog with test hooks.
    git-svn-id: https://svn.code.sf.net/p/boxi/code/trunk/boxi@604 bde4d9af-c013-0410-a270-fa18cafdf79f

---
+++ include/TestFileDialog.h
@@ -0,0 +1,94 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:        TestFileDialog.h
+// Purpose:     Test hook enabled version of wxFileDialog
+// Author:      Chris Wilson (originally Robert Roebling)
+// Modified by:
+// Created:     2008-08-20
+// Copyright:   (c) Robert Roebling, Chris Wilson
+// Old-RCS-ID:  $Id: filedlg.h 44027 2006-12-21 19:26:48Z VZ $
+// Licence:     wxWindows licence
+/////////////////////////////////////////////////////////////////////////////
+
+#ifndef _TESTFILEDIALOG_H
+#define _TESTFILEDIALOG_H
+
+#include "wx/filedlg.h"
+
+class TestFileDialog: public wxFileDialog
+{
+public:
+	TestFileDialog () : wxFileDialog() { }
+	TestFileDialog(wxWindow *parent,
+		const wxString& message = wxFileSelectorPromptStr,
+		const wxString& defaultDir = wxEmptyString,
+		const wxString& defaultFile = wxEmptyString,
+		const wxString& wildCard = wxFileSelectorDefaultWildcardStr,
+		long style = wxFD_DEFAULT_STYLE,
+		const wxPoint& pos = wxDefaultPosition,
+		const wxSize& sz = wxDefaultSize,
+		const wxString& name = wxFileDialogNameStr)
+	: wxFileDialog(parent, message, defaultDir, defaultFile, wildCard,
+		style, pos, sz, name)
+	{ }
+
+	virtual void SetPath(const wxString& path)
+	{
+		m_path = path;
+		this->wxFileDialog::SetPath(path);
+	}
+	
+	virtual void SetDirectory(const wxString& dir)
+	{
+		m_dir = dir;
+		this->wxFileDialog::SetDirectory(dir);
+	}
+	
+	virtual void SetFilename(const wxString& name)
+	{
+		m_fileName = name;
+		this->wxFileDialog::SetFilename(name);
+	}
+	
+	virtual wxString GetPath() const
+	{
+		if (wxGetApp().IsTestMode())
+		{
+			return m_path;
+		}
+		else
+		{
+			return this->wxFileDialog::GetPath();
+		}
+	}
+
+	// virtual void GetPaths(wxArrayString& paths) const { paths.Empty(); paths.Add(m_path); }
+	
+	virtual wxString GetDirectory() const
+	{
+		if (wxGetApp().IsTestMode())
+		{
+			return m_dir;
+		}
+		else
+		{
+			return this->wxFileDialog::GetDirectory();
+		}
+	}
+
+	virtual wxString GetFilename() const
+	{
+		if (wxGetApp().IsTestMode())
+		{
+			return m_fileName;
+		}
+		else
+		{
+			return this->wxFileDialog::GetFilename();
+		}
+	}
+
+	// DECLARE_DYNAMIC_CLASS(TestFileDialog)
+	DECLARE_NO_COPY_CLASS(TestFileDialog)
+};
+
+#endif // _TESTFILEDIALOG_H
