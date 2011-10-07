63e15151191a2a1cb59b8d9f1dfe333519bdea51
Chris Wilson <chris+github@qwirx.com>
2011-10-07 22:49:14 +0000

    Improve script and docs for building on Windows (using Cygwin).
    git-svn-id: https://svn.code.sf.net/p/boxi/code/trunk/boxi@719 bde4d9af-c013-0410-a270-fa18cafdf79f

--- configure-mingw.sh
+++ configure-mingw.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 if [ ! -x "configure" ]; then
-	if ! NOCONFIGURE=1 ./autogen.sh; then
+	if ! NOCONFIGURE=1 \
+		ACLOCAL_FLAGS='-I /usr/i686-pc-mingw32/share/aclocal' \
+		./autogen.sh
+	then
 		echo "Error: bootstrap failed, aborting." >&2
 		exit 1
 	fi
@@ -15,7 +18,9 @@ if ! ./configure --target=i686-pc-mingw32 \
 	CFLAGS="-mthreads" \
 	CXXFLAGS="-mthreads" \
 	LDFLAGS="-mthreads -L/usr/i686-pc-mingw32/lib" \
-	LIBS="-lcrypto -lws2_32 -lgdi32"
+	LIBS="-lcrypto -lws2_32 -lgdi32" \
+	--with-wx-config=/usr/i686-pc-mingw32/bin/wx-config \
+	--with-cppunit-prefix=/usr/i686-pc-mingw32
 then
 	echo "Error: configure failed, aborting." >&2
 	exit 1
