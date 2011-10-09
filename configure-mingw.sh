a45f5e6f2a95b2bb0bcc8d46ae848b8148a7874e
Chris Wilson <chris+github@qwirx.com>
2011-10-09 22:26:03 +0000

    Reuse Box Backup's MinGW configure script instead of duplicating most of it.
    git-svn-id: https://svn.code.sf.net/p/boxi/code/trunk/boxi@727 bde4d9af-c013-0410-a270-fa18cafdf79f

--- configure-mingw.sh
+++ configure-mingw.sh
@@ -15,12 +15,7 @@ fi
 export PKG_CONFIG_PATH="$DEP_PATH/lib/pkgconfig"
 LIBZ_PATH="$DEP_PATH/sys-root/mingw/lib"
 
-if ! ./configure "$@" --target=i686-pc-mingw32 \
-	CFLAGS="-mno-cygwin -mthreads" \
-	CPPFLAGS="-mno-cygwin" \
-	CXXFLAGS="-mno-cygwin -mthreads" \
-	LDFLAGS="-mno-cygwin -mthreads -L'$DEP_PATH/lib' -L'$LIBZ_PATH'" \
-	LIBS="-lcrypto -lws2_32 -lgdi32" \
+if ! ./boxbackup/infrastructure/mingw/configure.sh "$@" \
 	--with-wx-config="$DEP_PATH/bin/wx-config" \
 	--with-cppunit-prefix="$DEP_PATH"
 then
