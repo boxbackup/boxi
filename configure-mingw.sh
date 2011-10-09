3eb612164dd4c0301dd557301688c5cc19432860
Chris Wilson <chris+github@qwirx.com>
2011-10-09 22:17:23 +0000

    Search for libraries in the path where Cygwin installs MinGW zlib as well
    git-svn-id: https://svn.code.sf.net/p/boxi/code/trunk/boxi@726 bde4d9af-c013-0410-a270-fa18cafdf79f

--- configure-mingw.sh
+++ configure-mingw.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+DEP_PATH=/usr/i686-pc-mingw32
+
 if [ ! -x "configure" ]; then
 	if ! NOCONFIGURE=1 \
-		ACLOCAL_FLAGS='-I /usr/i686-pc-mingw32/share/aclocal' \
+		ACLOCAL_FLAGS="-I '${DEP_PATH}/share/aclocal'" \
 		./autogen.sh
 	then
 		echo "Error: bootstrap failed, aborting." >&2
@@ -10,16 +12,17 @@ if [ ! -x "configure" ]; then
 	fi
 fi
 
-export PKG_CONFIG_PATH=/usr/i686-pc-mingw32/lib/pkgconfig
+export PKG_CONFIG_PATH="$DEP_PATH/lib/pkgconfig"
+LIBZ_PATH="$DEP_PATH/sys-root/mingw/lib"
 
 if ! ./configure "$@" --target=i686-pc-mingw32 \
 	CFLAGS="-mno-cygwin -mthreads" \
 	CPPFLAGS="-mno-cygwin" \
 	CXXFLAGS="-mno-cygwin -mthreads" \
-	LDFLAGS="-mno-cygwin -mthreads -L/usr/i686-pc-mingw32/lib" \
+	LDFLAGS="-mno-cygwin -mthreads -L'$DEP_PATH/lib' -L'$LIBZ_PATH'" \
 	LIBS="-lcrypto -lws2_32 -lgdi32" \
-	--with-wx-config=/usr/i686-pc-mingw32/bin/wx-config \
-	--with-cppunit-prefix=/usr/i686-pc-mingw32
+	--with-wx-config="$DEP_PATH/bin/wx-config" \
+	--with-cppunit-prefix="$DEP_PATH"
 then
 	echo "Error: configure failed, aborting." >&2
 	exit 1
