cf6a7610836bcc1bf35454ca3b7a551d3d75b782
Chris Wilson <chris+github@qwirx.com>
2016-01-02 21:56:07 +0000

    Rename configure.in to configure.ac.
    Apparently the configure.in name is deprecated, and configure.ac is now
    preferred.
    
    Move some auto-generated crud into the infrastructure directory.
    
    Fix mismatch between autoconf and automake on aux dirs: AC_CONFIG_AUX_DIR
    works for automake, but autoconf ignores it, and can't find config.sub or
    config.guess in infrastructure/automake.
    
    Move assembled m4 files into infrastructure/automake/m4. Remove the old m4
    directory.

---
+++ infrastructure/update-main-h.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "Updating version in include/version.h"
+
+if [ -d .git ]; then
+	git_revision=`git rev-parse --short HEAD`
+	git_revision_number=".$git_revision"
+else
+	git_revision_number=""
+	git_revision="Not Git"
+fi
+
+sed -i \
+	-e "s/\(BOXI_VERSION \"[0-9.]*\).*\(\"\)/\1$git_revision_number\2/g" \
+	-e "s/\(BOXI_VERSION_ADVANCED\) .*/\1 \"$git_revision\"/g" \
+	-e "s/\(BOXI_VERSION_COMMAS \d+,\d+,\d+\),.*/\1,$git_revision_number\"/g" \
+	include/version.h
+
