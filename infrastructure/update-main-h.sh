1c629946b71d9d6b72a5fed16ecb784fb3f6ac43
Chris Wilson <chris+github@qwirx.com>
2016-01-03 22:19:21 +0000

    Really fix version.h for Git.

--- infrastructure/update-main-h.sh
+++ infrastructure/update-main-h.sh
@@ -3,15 +3,15 @@ echo "Updating version in include/version.h"
 
 if [ -d .git ]; then
 	git_revision=`git rev-parse --short HEAD`
-	git_revision_number=".$git_revision"
+	git_revision_number=",0x$git_revision"
 else
 	git_revision_number=""
 	git_revision="Not Git"
 fi
 
 sed -i \
-	-e "s/\(BOXI_VERSION \"[0-9.]*\).*\(\"\)/\1$git_revision_number\2/g" \
+	-e "s/\(BOXI_VERSION \"[0-9]\+\.[0-9]\+\.[0-9]\+\).*\(\"\)/\1 Git $git_revision_number\2/g" \
 	-e "s/\(BOXI_VERSION_ADVANCED\) .*/\1 \"$git_revision\"/g" \
-	-e "s/\(BOXI_VERSION_COMMAS \d+,\d+,\d+\),.*/\1,$git_revision_number\"/g" \
+	-e "s/\(BOXI_VERSION_COMMAS [0-9]\+,[0-9]\+,[0-9]\+\),.*/\1$git_revision_number/g" \
 	include/version.h
 
